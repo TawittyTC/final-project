d9ebb75ff148163686a920df13984db30233ca89
Tanapol Chamnanhan <tawitty.tc@gmail.com>
2023-02-15 20:53:11 +0700

    feat : Chatbot
    add chatbot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,4 +15,13 @@ export class AppComponent {
     this.modalService.open(modal);
   }
 
+  ngOnInit() {
+    (function(d, m){
+        var kommunicateSettings = {"appId":"3a8fabc0db6d4f8b8211417d48fa715e8","popupWidget":true,"automaticChatOpenOnNavigation":true};
+        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+        (window as any).kommunicate = m; m._globals = kommunicateSettings;
+    })(document, (window as any).kommunicate || {});
+  }
 }
