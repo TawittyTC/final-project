8e73f30f1879e809bcff3952abb43f424b4331a5
Tanapol Chamnanhan <tanapol.ch@rmutsvmail.com>
2023-09-27 15:07:24 +0700

    default-code

--- src/app/circle-chart/circle-chart.component.ts
+++ src/app/circle-chart/circle-chart.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { NgApexchartsModule } from "ng-apexcharts";
 
 @Component({
   selector: 'app-circle-chart',
