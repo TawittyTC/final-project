d3be5438937c660de3c99fd1f81e743b5b94dffd
Tanapol Chamnanhan <tanapol.ch@rmutsvmail.com>
2023-09-29 10:47:15 +0700

    update to scss in typescript

--- src/app/device-card/device-card.component.ts
+++ src/app/device-card/device-card.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-device-card',
   templateUrl: './device-card.component.html',
-  styleUrls: ['./device-card.component.css']
+  styleUrls: ['./device-card.component.scss']
 })
 export class DeviceCardComponent implements OnInit {
   deviceIds: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
