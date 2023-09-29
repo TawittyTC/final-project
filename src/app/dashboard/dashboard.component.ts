3e624ed7d9866e1bd217a14e2aa34f0958e21eb3
Tanapol Chamnanhan <tanapol.ch@rmutsvmail.com>
2023-09-29 11:38:24 +0700

    link ไปหน้า dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  device_id: string = ''; // กำหนดค่าเริ่มต้นเป็นสตริงว่าง
 
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    // ดึงค่า device_id จาก query parameter
+    const device_id = this.route.snapshot.queryParamMap.get('device_id');
+    if (device_id !== null) {
+      this.device_id = device_id; // กำหนดค่าเฉพาะเมื่อ device_id ไม่ใช่ null
+    }
+  }
 }
--- src/app/device-card/device-card.component.ts
+++ src/app/device-card/device-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-device-card',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./device-card.component.scss']
 })
 export class DeviceCardComponent implements OnInit {
-  deviceIds: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  @Input() device_ids: string[] = [];
+  device_id: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 }
