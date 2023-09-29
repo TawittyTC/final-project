288447545ac63a70de61b5dae219cc4ad4ab1d50
Tanapol Chamnanhan <tanapol.ch@rmutsvmail.com>
2023-09-29 11:57:11 +0700

    complete link dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,15 +7,11 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  device_id: string = ''; // กำหนดค่าเริ่มต้นเป็นสตริงว่าง
+  device_id: string = '';
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    // ดึงค่า device_id จาก query parameter
-    const device_id = this.route.snapshot.queryParamMap.get('device_id');
-    if (device_id !== null) {
-      this.device_id = device_id; // กำหนดค่าเฉพาะเมื่อ device_id ไม่ใช่ null
-    }
+    this.device_id = this.route.snapshot.queryParamMap.get('device_id') || '';
   }
 }
