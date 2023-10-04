8466f88500b0899a3fe5d90b1472982731e32b7a
Tanapol Chamnanhan <tanapol.ch@rmutsvmail.com>
2023-10-04 14:53:42 +0700

    Update device-card.component.ts

--- src/app/device-card/device-card.component.ts
+++ src/app/device-card/device-card.component.ts
@@ -1,32 +1,38 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {interval } from 'rxjs'
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-device-card',
   templateUrl: './device-card.component.html',
   styleUrls: ['./device-card.component.scss']
 })
-export class DeviceCardComponent implements OnInit {
-  device_id: string[] = []; // สร้างตัวแปรเก็บ device_id
-  latestDeviceData: any = {}; // สร้างตัวแปรเพื่อเก็บข้อมูลอุปกรณ์ล่าสุด
+export class DeviceCardComponent implements OnInit, OnDestroy {
+  device_id: string[] = [];
+  latestDeviceData: any = {};
+
+  private dataSubscription: Subscription | undefined;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.fetchData();
 
-    interval(1000).subscribe(()=>{
+    this.dataSubscription = interval(1000).subscribe(() => {
       this.fetchData();
     });
   }
 
+  ngOnDestroy(): void {
+    if (this.dataSubscription) {
+      this.dataSubscription.unsubscribe(); // เลิกรับข้อมูลเมื่อออกจากหน้า
+    }
+  }
+
   fetchData() {
     this.http.get<any[]>('http://localhost:3000/api/data').subscribe(data => {
-      console.log(data); // แสดงข้อมูลที่ได้รับจาก API ในคอนโซล
-      this.device_id = [...new Set(data.map(item => item.device_id))]; // รวบข้อมูล device_id เดียวกัน
+      this.device_id = [...new Set(data.map(item => item.device_id))];
 
-      // หาข้อมูลตัวล่าสุดจากคอลัมน์ id ในแต่ละ device_id
       this.latestDeviceData = {};
       this.device_id.forEach(id => {
         const filteredData = data.filter(item => item.device_id === id);
