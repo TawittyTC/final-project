827de6c2c81a73e2a89d0ff94c3fe287e13d9504
Thoth <kanansak555@gmail.com>
2023-10-30 11:30:31 +0700

    Update chart

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,189 +1,71 @@
-import { Component } from '@angular/core';
-import {
-  ApexAxisChartSeries,
-  ApexTitleSubtitle,
-  ApexDataLabels,
-  ApexFill,
-  ApexMarkers,
-  ApexYAxis,
-  ApexXAxis,
-  ApexTooltip,
-  ApexStroke
-} from "ng-apexcharts";
-
-export type ChartOptions = {
-  series: ApexAxisChartSeries;
-  chart: any; //ApexChart;
-  dataLabels: any;
-  markers: any;
-  title: ApexTitleSubtitle;
-  fill: ApexFill;
-  yaxis: any;
-  xaxis: any;
-  tooltip: any;
-  stroke: any;
-  grid: any; //ApexGrid;
-  colors: any;
-  toolbar: any;
-};
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';  // ต้องมีการ import HttpClient
+import { interval, Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.scss']
 })
-export class ChartComponent {
-  public chart1options: Partial<ChartOptions> | any;
-  public chart2options: Partial<ChartOptions> | any;
-  public chart3options: Partial<ChartOptions> | any;
-  public commonOptions: Partial<ChartOptions> = {
-    dataLabels: {
-      enabled: false
-    },
-    stroke: {
-      curve: "straight"
-    },
-    toolbar: {
-      tools: {
-        selection: false
-      }
-    },
-    markers: {
-      size: 6,
-      hover: {
-        size: 10
-      }
-    },
-    tooltip: {
-      followCursor: false,
-      theme: "dark",
-      x: {
-        show: false
-      },
-      marker: {
-        show: false
-      },
-      y: {
-        title: {
-          formatter: function () {
-            return "";
+export class ChartComponent implements OnInit {
+  public chartData: any[] = [];
+  public chartOptions: any;
+  device_id: string = '';
+
+  constructor(private route: ActivatedRoute,private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.device_id = this.route.snapshot.queryParamMap.get('device_id') || '';
+    this.fetchData();
+      // เรียกเมธอดสร้างกราฟ
+      //this.generateChart();
+  }
+
+  generateChart() {
+    // ทำการสร้างและกำหนดตัวเลือกสำหรับกราฟโดยใช้ ng-apexcharts
+    this.chartOptions = {
+      series: [{
+        name: "Energy",
+        data: this.chartData.map(item => ({
+          x: new Date(item.created_timestamp).getTime(),
+          y: item.energy
+        }))
+      }],
+      chart: {
+        type: 'line',
+        height: 350,
+        animations: {
+          enabled: true,
+          easing: 'linear',
+          dynamicAnimation: {
+            speed: 2000
           }
         }
-      }
-    },
-    grid: {
-      clipMarkers: false
-    },
-    xaxis: {
-      type: "datetime"
-    }
-  };
-
-  constructor() {
-    this.initCharts();
-  }
-
-  public initCharts(): void {
-    this.chart1options = {
-      series: [
-        {
-          name: "chart1",
-          data: this.generateDayWiseTimeSeries(
-            new Date("11 Feb 2017").getTime(),
-            20,
-            {
-              min: 10,
-              max: 60
-            }
-          )
-        }
-      ],
-      chart: {
-        id: "fb",
-        group: "social",
-        type: "line",
-        height: 160
       },
-      colors: ["#008FFB"],
-      yaxis: {
-        tickAmount: 2,
+      xaxis: {
+        type: 'datetime',
         labels: {
-          minWidth: 40
+          format: 'dd MMM yyyy'
         }
-      }
-    };
-
-    this.chart2options = {
-      series: [
-        {
-          name: "chart2",
-          data: this.generateDayWiseTimeSeries(
-            new Date("11 Feb 2017").getTime(),
-            20,
-            {
-              min: 10,
-              max: 30
-            }
-          )
-        }
-      ],
-      chart: {
-        id: "tw",
-        group: "social",
-        type: "line",
-        height: 160
       },
-      colors: ["#546E7A"],
       yaxis: {
-        tickAmount: 2,
-        labels: {
-          minWidth: 40
-        }
-      }
-    };
-
-    this.chart3options = {
-      series: [
-        {
-          name: "chart3",
-          data: this.generateDayWiseTimeSeries(
-            new Date("11 Feb 2017").getTime(),
-            20,
-            {
-              min: 10,
-              max: 60
-            }
-          )
-        }
-      ],
-      chart: {
-        id: "yt",
-        group: "social",
-        type: "area",
-        height: 160
-      },
-      colors: ["#00E396"],
-      yaxis: {
-        tickAmount: 2,
-        labels: {
-          minWidth: 40
+        title: {
+          text: 'Energy'
         }
       }
     };
   }
 
-  public generateDayWiseTimeSeries(baseval: number, count: number, yrange: { min: any; max: any; }): any[] {
-    let i = 0;
-    let series = [];
-    while (i < count) {
-      var x = baseval;
-      var y =
-        Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
-
-      series.push([x, y]);
-      baseval += 86400000;
-      i++;
-    }
-    return series;
+  fetchData() {
+    // ดึงข้อมูลจาก API โดยใช้ค่า device_id จากตัวแปร
+    this.http.get<any[]>(`http://localhost:3000/energy?device_id=${this.device_id}`).subscribe(data => {
+      this.chartData = data;
+      console.log(this.chartData)
+      // เรียกเมธอดสร้างกราฟ
+      this.generateChart();
+    });
   }
+  
 }
