ea0da43f9977d09077784467625b3fc9c6c61a1a
Thoth <kanansak555@gmail.com>
2023-11-07 15:55:07 +0700

    update signup

--- src/app/service/user.ts
+++ src/app/service/user.ts
@@ -1,6 +1,7 @@
 export class User {
     id!: number;
     name: String= '';
+    lname: String= '';
     email: String = '';
     password: String= '';
     role: string='';
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
-
+import { AuthService } from '../service/auth.service';
+import { User } from '../service/user';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
+  user: User = new User(); // Initialize an empty user object
+  errorMessage: string = ''; // To display error messages
 
+  constructor(private authService: AuthService) {}
+
+  signUp() {
+    console.log('Signing up with user data:', this.user); // Debugging statement
+    
+    this.authService.signUp(this.user).subscribe(
+      (result) => {
+        console.log('Registration API response:', result); // Debugging statement
+        if (result) {
+          console.log('Registration successful');
+          // You can optionally redirect the user to a different page upon successful registration
+          // Example: this.router.navigate(['/success-page']);
+        } else {
+          this.errorMessage = 'Registration failed. Please try again.';
+        }
+      },
+      (error) => {
+        console.error('Error during registration:', error); // Debugging statement
+        this.errorMessage = 'An error occurred while registering. Please try again later.';
+      }
+    );
+  }
 }
