996473b07543a208d47a94bf59fe9916cc79281a
Thoth <kanansak555@gmail.com>
2023-11-08 14:42:31 +0700

    update Check token expiration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,7 @@ import { AuthService } from './service/auth.service';
 })
 export class AppComponent {
   title = 'final-project';
-  constructor(private modalService: NgbModal,private authService: AuthService) {
-  }
+  constructor(private modalService: NgbModal, private authService: AuthService) {}
 
   public open(modal: any): void {
     this.modalService.open(modal);
@@ -20,6 +19,9 @@ export class AppComponent {
     this.authService.tokenExpiration$.subscribe(() => {
       // Display a notification to the user that the token is about to expire
       alert('Your session is about to expire. Please log in again.');
+
+      // Perform a hard refresh to reload the page
+      location.reload();
     });
   }
 }
